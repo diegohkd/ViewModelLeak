0776141634159e2ab4e5805c3de44541edf11521
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-11-10 15:07:40 -0300

    Refactor: remove unused Listener

--- leak/src/main/java/example/com/viewmodelleak/Repository.kt
+++ leak/src/main/java/example/com/viewmodelleak/Repository.kt
@@ -3,22 +3,12 @@ package example.com.viewmodelleak
 object Repository {
 
     private var requestCallback: (() -> Unit)? = null
-    private var listener: Listener? = null
 
     fun performFakeRequest(callback: () -> Unit) {
         requestCallback = callback
     }
 
-    fun performFakeRequest(listener: Listener) {
-        this.listener = listener
-    }
-
     fun finishFakeRequest() {
         requestCallback?.invoke()
-        listener?.callback()
     }
-}
-
-interface Listener{
-    fun callback()
 }
